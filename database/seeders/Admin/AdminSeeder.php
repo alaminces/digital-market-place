439ea24b3b77ff33fb4ac7e1b5e015bedbe03585
alamin-miah <alaminces6@gmail.com>
2025-07-28 11:43:13 +0600

    create admin seeder

---
+++ database/seeders/Admin/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders\Admin;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // admin data
+        $admin = new Admin();
+        $admin->name = 'Admin';
+        $admin->email = 'admin@gmail.com';
+        $admin->password = bcrypt('1234');
+        $admin->save();
+    }
+}
