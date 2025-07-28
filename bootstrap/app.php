c38703cf4202a52679b5c7f63b983fff431ef1c6
alamin-miah <alaminces6@gmail.com>
2025-07-28 13:34:29 +0600

    working with auth middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
+            'auth' => Illuminate\Auth\Middleware\Authenticate::class,
             'guest' => RedirectIfAuthenticated::class,
         ]);
     })
